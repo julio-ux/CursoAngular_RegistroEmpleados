d1b448d8b4d415ed07c49ff02af3a38a0d59bc0c
julio_castillo <julio_castillo19922005@outlook.com>
2021-12-27 20:12:10 -0600

    Video 19 Comunicacion entre componentes INPUT

---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleados } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent implements OnInit {
+
+  @Input() empleadoDeLista: Empleados;
+  @Input() indice: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
